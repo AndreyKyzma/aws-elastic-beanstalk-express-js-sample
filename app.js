0de3a7555a8371cabef034d6c1506e3d42548ea4
Andrey <andreykyzmenkoo@gmail.com>
2021-04-14 18:25:36 +0300

    Update app.js

--- app.js
+++ app.js
@@ -4,9 +4,8 @@ const port = 8080;
 
 <<<<<<< HEAD
 app.get('/', (req, res) => res.send('Hello Andrey!'));
-=======
-app.get('/', (req, res) => res.send('Hello World Andrey!'));
->>>>>>> 1c3ead24713e303a93c7ada4015de8991afcfb81
+
+
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
